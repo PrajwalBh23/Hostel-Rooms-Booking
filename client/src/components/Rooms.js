3bb05c5593e3fcecd0a1fac2104b6e4e12e26539
Prajwal <pbhoyar622@gamil.com>
2023-10-02 13:21:18 +0530

    Fivth Commit

--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -4,6 +4,8 @@ import { Box, Typography } from "@mui/material";
 import Header from './Header.js';
 import './Rooms.css';
 import Checkbox from '@mui/material/Checkbox';
+import house from '../images/House_sample.jpg'
+import VerifiedRoundedIcon from '@mui/icons-material/VerifiedRounded';
 
 function Rooms() {
     const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
@@ -51,27 +53,39 @@ function Rooms() {
                                 <div className="head">Including Bills</div>
                                 <ul>
                                     <li><Checkbox {...label} />Electricity</li>
-                                    <li><Checkbox {...label} />Water</li> 
+                                    <li><Checkbox {...label} />Water</li>
                                 </ul>
                             </div>
                         </div>
                     </Typography>
                     <Typography sx={{ fontFamily: "fantasy" }} width={"75%"} variant="h2">
                         <div className="result">
-                            This is Home Page
-                            this is filter block
-                        </div>
-                        <div className="result">
-                            This is Home Page
-                            this is filter block
-                        </div>
-                        <div className="result">
-                            This is Home Page
-                            this is filter block
-                        </div>
-                        <div className="result">
-                            This is Home Page
-                            this is filter block
+                            <div className="box">
+                                <img src={house} alt="not showing" />
+                                <div className="info">
+                                    <div className="price">₹10000-12000</div>
+                                    <div className="address">House No. 9 Mahada Colony, Butibori Nagpur</div>
+                                    <div className="buttons">
+                                        <button>Shared +4</button>
+                                        <button>Semi-Furnished</button>
+                                        <button>Only Girls</button>
+                                    </div>
+                                    <div className="extra">2 Bedrooms with Separate Common Area & Seprate Washroom</div>
+                                    <div className="availablity">Available Now<button>10+ Years Experience</button></div>
+                                </div>
+                                <div className="map">
+                                    <img src={house} alt="" />
+                                </div>
+                            </div>
+                            <div className="block">
+                                <div className="owner">
+                                    <span><VerifiedRoundedIcon /></span>
+                                    <span>Mansi Pillewan</span>
+                                </div>
+                                <div className="corner">
+                                    <span>View More</span>
+                                </div>
+                            </div>
                         </div>
                     </Typography>
                 </Box>
