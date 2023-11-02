cabf87325af9f1efdd5fc31b91a9029d089479fb
Prajwal <pbhoyar622@gamil.com>
2023-11-02 22:55:36 +0530

    Backend Seventh Commit

--- Backned/controllers/auth.js
+++ Backned/controllers/auth.js
@@ -2,38 +2,62 @@ import User from '../model/Users.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-export const register = async(req,res,next)=>{
+export const register = async (req, res, next) => {
     try {
         const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password, salt)
+        const hash = bcrypt.hashSync(req.body.password, salt);
 
-        if(!name || !email || !password || !phone || !Username){
-            return res.status(422).json({error: "plz fill it properly"})
+        // Check if all required fields are provided
+        if (!req.body.name || !req.body.email || !req.body.password || !req.body.phone || !req.body.Username) {
+            return res.status(422).json({ error: "P fill in all the fields properly" });
         }
 
         const newUser = new User({
             Username: req.body.Username,
-            email:req.body.email,
+            email: req.body.email,
             name: req.body.name,
-            phone:req.body.phone,
-            password:hash
-        })
-        await newUser.save()
-        res.status(200).send("User has been Created");
+            phone: req.body.phone,
+            password: hash,
+        });
+        await newUser.save();
+        res.status(200).send("User has been created");
     } catch (error) {
         next(error);
     }
-} 
+}
 
-export const login = async(req,res,next)=>{
+export const login = async (req, res, next) => {
     try {
-        const user = await User.findOne({Username:req.body.Username})
-        if(!user) return res.status(404).json({ error: "Invalid Credential" });
- 
-        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-        if(!isPasswordCorrect) return res.status(404).json({ error: "Invalid Credential" });
-        
-        res.status(200).json(user);
+        let token;
+        const {Username, password} = req.body;
+
+        if(!Username || !password){
+            return res.status(400).json({error:'Please filled the data'});
+        }
+        const user = await User.findOne({ Username: req.body.Username });
+        if (user) 
+        {
+            const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+            if (isPasswordCorrect) 
+            {
+                token = await user.generateAuthToken();
+                console.log(token);
+
+                res.cookie("JwtToken", token,{
+                    expires: new Date(Date.now() + 259200000)
+                });
+
+                res.status(200).json({message:"User Sign in Successfully"});
+            }
+            else{
+                return res.status(404).json({ error: "Invalid Credentials" });
+            }
+        }
+        else
+        {
+            return res.status(404).json({ error: "Invalid Credentials" });
+        }
+
     } catch (error) {
         next(error);
     }
--- Backned/model/Users.js
+++ Backned/model/Users.js
@@ -1,43 +1,60 @@
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
 // import { text } from "express";
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    Username:{
+    Username: {
         type: String,
         required: true,
         unique: true
     },
-    name:{
+    name: {
         type: String,
         required: true,
         unique: true
     },
-    phone:{
+    phone: {
         type: Number,
-        required:true,
-        unique:true
+        required: true,
+        unique: true
     },
-    phone2:{
+    phone2: {
         type: Number
     },
-    email:{
+    email: {
         type: String,
         required: true,
-        unique:true
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     },
-    // verify:{
-    //     type: String,
-    // },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true,
+            }
+        }
+    ]
 });
 
 // to search in the name and description.
 // hotelSchema.index({ name:'text', description : 'text' })
 
+// Generation of token
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token: token});
+        await this.save();
+        return token;
+    } catch (err) {
+        console.log(err);
+    }
+}
 
-export default mongoose.model("User",userSchema);
+export default mongoose.model("User", userSchema);
